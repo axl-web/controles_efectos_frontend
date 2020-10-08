4fd60d391524a320da923618fdc014a10ad4d19e
axl-web <montenegroaxel@outlook.com>
2020-10-08 20:19:56 -0300

    animation autocomplete drag & filter

---
+++ filters/main.js
@@ -0,0 +1,10 @@
+(function(){
+    mixitup('.container',{
+        "animation": {
+            "duration": 572,
+            "nudge": true,
+            "reverseOut": false,
+            "effects": "fade rotateY(180deg)"
+        }
+    })
+})()
